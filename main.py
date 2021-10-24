eac94ab124ac3645759e3155c01dddcf12c70f62
Pinkac1425 <81621903+Pinkac1425@users.noreply.github.com>
2021-10-24 18:43:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+vysledky = ["1:2","5:5","7:3","4:2","1:2"]
+
+janove_tipy = [1,6,5,8]
+tomasove_tipy = [2,8,9]
+petrove_tipy = [7,9,8,12]
+moje_tipy = [5,2,1,3,10]
+
+pocet_strelenych_golov = [3,10,6]
+pocet_strelenych_golov = []
+
+
+for vysledok in vysledky:
+    zoznam = vysledok.split(":")
+    pocet_strelenych_golov = int(zoznam[0]) + int(zoznam[1])
+
+if moje_tipy in vysledky:
+
+
+
+
+
+
+
+
+
